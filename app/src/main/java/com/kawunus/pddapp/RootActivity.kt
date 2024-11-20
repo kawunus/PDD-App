7fb3f6a82ff553f2046910dcf111465cf5a0f9dc
stanislau <s.markovtsev@gmail.com>
2024-11-21 00:32:26 +0300

    Подключил viewBinding и поменял название с MainActivity.kt на RootActivity.kt

---
+++ app/src/main/java/com/kawunus/pddapp/RootActivity.kt
@@ -0,0 +1,14 @@
+package com.kawunus.pddapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
