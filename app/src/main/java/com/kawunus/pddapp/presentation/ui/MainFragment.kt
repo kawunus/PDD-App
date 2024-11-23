fdff9542cdafaa139ec2c31f438ff81df8c40996
stanislau <s.markovtsev@gmail.com>
2024-11-24 01:15:42 +0300

    Добавил библиотеку Jetpack Navigation + покрасил 4-ю секцию в зеленый

---
+++ app/src/main/java/com/kawunus/pddapp/presentation/ui/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.kawunus.pddapp.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kawunus.pddapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
